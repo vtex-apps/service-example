9f1709e0bc62bc45ec0959f428a967d0ed1d5925
Guilherme Rodrigues <gadr90@gmail.com>
2019-07-06 11:15:01 -0300

    Benchmark

--- node/index.ts
+++ node/index.ts
@@ -1,53 +1,22 @@
-import { ClientsConfig, LRUCache, Service, ServiceContext } from '@vtex/api'
+import { Service } from '@vtex/api'
 
-import { Clients } from './clients'
-import { method } from './middlewares/method'
-import { status } from './middlewares/status'
-import { validate } from './middlewares/validate'
-
-const TIMEOUT_MS = 800
-
-// Create a LRU memory cache for the Status client.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({max: 5000})
-metrics.trackCache('status', memoryCache)
-
-// This is the configuration for clients available in `ctx.clients`.
-const clients: ClientsConfig<Clients> = {
-  // We pass our custom implementation of the clients bag, containing the Status client.
-  implementation: Clients,
-  options: {
-    // All IO Clients will be initialized with these options, unless otherwise specified.
-    default: {
-      retries: 2,
-      timeout: TIMEOUT_MS,
-    },
-    // This key will be merged with the default options and add this cache to our Status client.
-    status: {
-      memoryCache,
+export default new Service({
+  graphql: {
+    resolvers: {
+      Query: {
+        test: () => ({
+          message: 'graphql',
+        }),
+      },
     },
   },
-}
-
-declare global {
-  // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
-  type Context = ServiceContext<Clients, State>
-
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
-  interface State {
-    code: number
-  }
-}
-
-// Export a service that defines route handlers and client options.
-export default new Service<Clients, State>({
-  clients,
   routes: {
-    // `status` is the route ID from service.json. It maps to an array of middlewares (or a single handler).
-    status: [
-      method,
-      validate,
-      status,
-    ],
+    test: (ctx) => {
+      // ctx.set('transfer-encoding', 'chunked')
+      ctx.set('cache-control', 'no-cache, no-store')
+      ctx.body = {
+        message: 'http',
+      }
+    }
   },
 })
--- node/middlewares/method.ts
+++
@@ -1,8 +0,0 @@
-export const method = async (ctx: Context, next: () => Promise<any>) => {
-  if (ctx.method.toUpperCase() !== 'GET') {
-    ctx.status = 405
-    return
-  }
-
-  await next()
-}
