f56f907a50b5655865c23b38096c0802adc93d2a
tiagonapoli <tiago.napoli@vtex.com.br>
2019-10-23 13:30:11 -0300

    remove all tests

--- node/__tests__/simple.test.ts
+++ node/__tests__/simple.test.ts
@@ -2,8 +2,8 @@
 //
 // More info: https://github.com/sapegin/jest-cheat-sheet
 
-describe('simpleTest', () => {
-  it('one test case', async () => {
-    expect(0.2 + 0.1).not.toBe(0.3)
-  })
-})
+// describe('simpleTest', () => {
+//   it('one test case', async () => {
+//     expect(0.2 + 0.1).not.toBe(0.3)
+//   })
+// })
