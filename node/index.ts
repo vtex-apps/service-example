a1f23bbe2b9caa54b44b2999292a1b47f85d49c9
tiagonapoli <tiago.napoli@vtex.com.br>
2019-12-27 19:22:40 -0300

    simplify

--- node/index.ts
+++ node/index.ts
@@ -1,58 +1,15 @@
-import { ClientsConfig, LRUCache, Service, ServiceContext } from '@vtex/api'
-import { Clients } from './clients'
-import { method } from './middlewares/method'
-import { status } from './middlewares/status'
-import { validate } from './middlewares/validate'
-import { a } from '@tiagonapoli/test-pkg-a'
-import { b } from '@tiagonapoli/test-pkg-b'
+import { a } from "@tiagonapoli/test-pkg-a";
+import { b } from "@tiagonapoli/test-pkg-b";
+import { Service, ServiceContext } from "@vtex/api";
 
-console.log("IMPORT DE A", a)
-console.log("IMPORT DE B", b)
+console.log("IMPORT DE A", a);
+console.log("IMPORT DE B", b);
 
-
-const TIMEOUT_MS = 800
-
-// Create a LRU memory cache for the Status client.
-// The @vtex/api HttpClient respects Cache-Control headers and uses the provided cache.
-const memoryCache = new LRUCache<string, any>({max: 5000})
-metrics.trackCache('status', memoryCache)
-
-// This is the configuration for clients available in `ctx.clients`.
-const clients: ClientsConfig<Clients> = {
-  // We pass our custom implementation of the clients bag, containing the Status client.
-  implementation: Clients,
-  options: {
-    // All IO Clients will be initialized with these options, unless otherwise specified.
-    default: {
-      retries: 2,
-      timeout: TIMEOUT_MS,
-    },
-    // This key will be merged with the default options and add this cache to our Status client.
-    status: {
-      memoryCache,
-    },
-  },
-}
-
-declare global {
-  // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
-  type Context = ServiceContext<Clients, State>
-
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
-  interface State {
-    code: number
-  }
-}
-
-// Export a service that defines route handlers and client options.
-export default new Service<Clients, State>({
-  clients,
+export default new Service({
   routes: {
-    // `status` is the route ID from service.json. It maps to an array of middlewares (or a single handler).
-    status: [
-      method,
-      validate,
-      status,
-    ],
-  },
-})
+    test: (ctx: ServiceContext) => {
+      ctx.body = "test";
+      ctx.status = 200;
+    }
+  }
+});
--- node/middlewares/method.ts
+++
@@ -1,8 +0,0 @@
-export async function method(ctx: Context, next: () => Promise<any>) {
-  if (ctx.method.toUpperCase() !== 'GET') {
-    ctx.status = 405
-    return
-  }
-
-  await next()
-}
