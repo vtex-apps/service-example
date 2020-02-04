bd80baa0d4b8434bd2a3ab7c21b6d98d307a62ae
dhasuda <davihasuda@gmail.com>
2020-02-04 14:42:58 -0300

    Testing :see_no_evil:

--- node/middlewares/status.ts
+++ node/middlewares/status.ts
@@ -3,7 +3,7 @@ export async function status(ctx: Context, next: () => Promise<any>) {
   const {state: {code}, clients: {status: statusClient}} = ctx
 
   const {headers, data, status: responseStatus} = await statusClient.getStatusWithHeaders(code)
-  console.log('CONTEXT VTEX', ctx.vtex)
+  console.log('CONTEXT VTEX', JSON.stringify(ctx.vtex.settings))
 
   ctx.status = responseStatus
   ctx.body = data
