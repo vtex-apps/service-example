ae1cdfb001457176746105ea98b28168536226ba
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-07-01 16:45:22 -0300

    asdf
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

---
+++ node/index.test.ts
@@ -0,0 +1,11 @@
+// // You can import your files here for testing them
+// //
+// // More info: https://github.com/sapegin/jest-cheat-sheet
+
+export const a = 5
+
+describe('simpleTest', () => {
+  it('one test case', async () => {
+    expect(0.2 + 0.1).not.toBe(0.3)
+  })
+})
