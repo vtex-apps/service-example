18976157017cb6eee450942d6360553b3e039a0f
AMIT <amit.k@trikatechnologies.com>
2023-08-01 13:46:38 +0530

    WT-265: changed the color and size ids

--- react/index.tsx
+++ react/index.tsx
@@ -1,13 +1,10 @@
 import canUseDOM from 'vtex.render-runtime'
 
 declare const window: any
-const categoryId = {
-  apparel: "502",
-  footwear: "513"
-}
 
-const colorIds = [854];
-const sizeIds = [885, 843, 861, 842]
+const allowedCategoryIds = ["554","609","610","611","612","559","555","586","670","671","672","589","685"];
+const colorIds = [613];
+const sizeIds = [619, 623]
 
 export function handleEvents(e: any) {
 
@@ -34,32 +31,41 @@ export function handleEvents(e: any) {
       // Don't change anything above this line
     })('wgs', 'staging', {autoCalculate: false});
 
-    const products = e.data.transactionProducts.filter((item: any) => (item.categoryIdTree.includes(categoryId.apparel) || item.categoryIdTree.includes(categoryId.footwear)));
+    const products = e.data.transactionProducts.filter((item: any) => {
+      let categoryIdObject :any = {}
+      allowedCategoryIds.forEach((categoryId:any) => {
+        if(item.categoryIdTree.includes(categoryId)){
+          categoryIdObject[categoryId] = categoryId
+          }
+        })
+        return Boolean(Object.keys(categoryIdObject).length)
+    });
     const skuIds = products.map((product: any) => product.sku).toString();
-
-    fetch("/v0/get-product-details?skuIds=" + skuIds).then(res => res.json())
-    .then((skuSpecifications) => {
-      const skuItems = skuSpecifications.map((item: any) => ({
-        color: item.find((skuItem: any) => (colorIds.includes(skuItem.FieldId)))?.Text,
-        size: item.find((skuItem: any) => (sizeIds.includes(skuItem.FieldId)))?.Text
-      }))
-      window.tfcapi('track', 'checkout', {
-          userId: '',
-          orderId: e.data.transactionId,
-          locale: 'en_US',
-          products: products.map((product: any, index: any) => ({
-            productId: product.productRefId,
-            colorId: skuItems[index]?.color,
-            quantity: product.quantity,
-            price: product.price,
-            currency: e.data.transactionCurrency,
-            size: skuItems[index]?.size,
-            sku: product.sku
-          }))
-      });
-    }).catch((error) => {
-      console.log(error);
-    })
+    if(skuIds.length){
+      fetch("/v0/get-product-details?skuIds=" + skuIds).then(res => res.json())
+      .then((skuSpecifications) => {
+        const skuItems = skuSpecifications.map((item: any) => ({
+          color: item.find((skuItem: any) => (colorIds.includes(skuItem.FieldId)))?.Text,
+          size: item.find((skuItem: any) => (sizeIds.includes(skuItem.FieldId)))?.Text
+        }))
+        window.tfcapi('track', 'checkout', {
+            userId: '',
+            orderId: e.data.transactionId,
+            locale: 'en_US',
+            products: products.map((product: any, index: any) => ({
+              productId: product.productRefId,
+              colorId: skuItems[index]?.color,
+              quantity: product.quantity,
+              price: product.price,
+              currency: e.data.transactionCurrency,
+              size: skuItems[index]?.size,
+              sku: product.sku
+            }))
+        });
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
    }
 }
 
